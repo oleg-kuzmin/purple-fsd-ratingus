f1067303746adcc2be623f9e3af2668eae8689b0
Oleg Kuzmin <info@oleg-kuzmin.ru>
2026-05-10 18:57:28 +0300

    4.10. Layout

---
+++ src/app/layout/Layout.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router';
+
+export function Layout() {
+  return (
+    <div>
+      <div>Шапка</div>
+      <Outlet />
+      <div>Футер</div>
+    </div>
+  );
+}
--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from 'react-router';
 import { CategoryPage } from '../../pages/category';
+import { Layout } from '../layout';
 
 export const router = createBrowserRouter([
   {
     path: '/',
+    Component: Layout,
     children: [
       {
         index: true,
