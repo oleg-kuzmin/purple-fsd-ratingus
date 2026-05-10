d9132bf2ebe9aee22480e87ff49084098353352f
Oleg Kuzmin <info@oleg-kuzmin.ru>
2026-05-10 13:42:34 +0300

    4.3. Подготовка слоя App

--- src/app/entrypoint/main.tsx
+++ src/app/entrypoint/main.tsx
@@ -1,13 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <div>Привет из FSD</div>,
-  },
-]);
+import { RouterProvider } from 'react-router';
+import { router } from '../routes';
+import '../styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
