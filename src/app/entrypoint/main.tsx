5ff72813ed13884882d21d82441f9391b9c5ca48
Oleg Kuzmin <info@oleg-kuzmin.ru>
2026-05-10 18:41:03 +0300

    4.9. Добавление стилей

--- src/app/entrypoint/main.tsx
+++ src/app/entrypoint/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import { router } from '../routes';
-import '../styles/index.css';
+import '../styles/variables.css';
+import '../styles/base.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
