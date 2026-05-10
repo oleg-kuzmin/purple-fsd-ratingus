099d43c9a51165beaa1fe03cfdcba348b1b64dfc
Oleg Kuzmin <info@oleg-kuzmin.ru>
2026-05-10 14:23:11 +0300

    4.6. Структура роутинга

--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -3,6 +3,30 @@ import { createBrowserRouter } from 'react-router';
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Привет из FSD</div>,
+    children: [
+      {
+        index: true,
+        element: <a>Main</a>,
+      },
+      {
+        path: 'community',
+        element: <a>Community</a>,
+      },
+      {
+        path: ':category',
+        element: <a>Category</a>,
+      },
+      {
+        path: 'article',
+        children: [{ path: ':alias', element: <a>Article</a> }],
+      },
+      {
+        path: 'profile',
+        children: [
+          { index: true, element: <a>Profile</a> },
+          { path: 'edit', element: <a>EditProfile</a> },
+        ],
+      },
+    ],
   },
 ]);
