2ecc07060131faf21185bfd3a39a58241739c912
Oleg Kuzmin <info@oleg-kuzmin.ru>
2026-05-23 15:48:18 +0300

    8.3. Специфичность в СSS

--- src/features/article/ui/Like/Like.tsx
+++ src/features/article/ui/Like/Like.tsx
@@ -1,3 +1,12 @@
+import { Tag } from '@/shared/ui';
+import styles from './Like.module.css';
+
 export function Like() {
-  return <div>Like</div>;
+  return (
+    <div>
+      <Tag size="m" color="orange" icon="Check" className={styles.like}>
+        Like
+      </Tag>
+    </div>
+  );
 }
--- src/pages/main/ui/MainPage/MainPage.tsx
+++ src/pages/main/ui/MainPage/MainPage.tsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router';
 import { ROUTES } from '@/shared/routes';
+import { Like } from '@/features/article';
 
 export function MainPage() {
   return (
     <div>
       Главная
       <Link to={ROUTES.profile.index}>Профиль</Link>
+      <Like />
     </div>
   );
 }
